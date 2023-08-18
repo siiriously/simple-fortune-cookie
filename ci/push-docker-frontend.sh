2c1b994b850f8ed932cc30571f810cb6886a3986
FinnKramer <fikra23@student.sdu.dk>
2023-08-18 07:31:46 +0000

    refactored bash scripts to make github username lowercase

--- ci/push-docker-frontend.sh
+++ ci/push-docker-frontend.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
 docker push "ghcr.io/$docker_username/frontend:1.0-${GIT_COMMIT::8}" 
 docker push "ghcr.io/$docker_username/frontend:latest" &
