92a879733ffa6271e7b8e0a76889e148d8dd682b
FinnKramer <fikra23@student.sdu.dk>
2023-08-18 07:43:39 +0000

    REFACTOR docker build-push script, use the lowercase version of username

--- ci/push-docker-frontend.sh
+++ ci/push-docker-frontend.sh
@@ -1,5 +1,5 @@
 docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
-echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/frontend:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/frontend:latest" &
+echo "$docker_username_lower" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
+docker push "ghcr.io/$docker_username_lower/frontend:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username_lower/frontend:latest" &
 wait
